69e7f2b9af4e965d85286827d8602e689ca39463
Trần Trung Tuấn <90963865+TuanTranCauAm@users.noreply.github.com>
2024-03-19 00:56:49 +0700

    Create user data

---
+++ App/Assets/Script/Model/UserDataAsset.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "UserDataAsset", menuName = "ScriptableObjects/UserDataAsset")]
+class UserDataAsset: ScriptableObject
+{
+    public string dataFileName;
+    private string accessToken;
+
+    public string AccessToken
+    {
+        get { return accessToken; }
+        set { accessToken = value;  }
+    }
+}
+

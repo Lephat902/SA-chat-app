2d35db61edd1e7f04e217b9c2e2b5dda8c447e7e
Trần Trung Tuấn <90963865+TuanTranCauAm@users.noreply.github.com>
2024-03-19 00:04:28 +0700

    Call api success

---
+++ App/Assets/Script/Model/BackEnd/CustomHTTP.SignUp.cs
@@ -0,0 +1,41 @@
+﻿using Cysharp.Threading.Tasks;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public struct FailRequest
+{
+    public List<string> message;
+}
+
+public struct SignUpModel
+{
+    public string username;
+    public string password;
+    public string avatar;
+}
+
+public struct SignInModel
+{
+    public string username;
+    public string password;
+}
+
+
+public static partial class CustomHTTP
+{
+    public static async void SignUp(string url, object data, Action result, Action<FailRequest> error)
+    {
+        var response = await POST(url, data);
+
+        var content = await response.Content.ReadAsStringAsync();
+
+        Debug.Log("Result: " + JsonUtility.ToJson(content));
+
+        if (!response.IsSuccessStatusCode)
+            error.Invoke(JsonUtility.FromJson<FailRequest>(content));
+        else
+            result.Invoke();
+    }
+}
---
+++ App/Assets/Script/Model/BackEnd/CustomHTTP.cs
@@ -0,0 +1,22 @@
+﻿using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public static partial class CustomHTTP
+{
+    public const string DOMAIN = "https://chatapp.tutorify.site";
+
+    private static async Task<HttpResponseMessage> POST(string url, object data)
+    {
+        using (var client = new HttpClient())
+        {
+            var jsonData = JsonUtility.ToJson(data);
+            var respone = await client.PostAsync(url,
+                new StringContent(jsonData, Encoding.UTF8, "application/json"));
+
+            Debug.Log("POST to " + url + " data: " + jsonData);
+            return respone;
+        }
+    }
+}

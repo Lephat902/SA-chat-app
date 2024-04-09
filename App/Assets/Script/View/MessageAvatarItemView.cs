534d0a8c77d155928dd6b21b70c19e197a531ccf
TuanWolffun <103925020+TuanWolffun@users.noreply.github.com>
2024-04-09 11:05:11 +0700

    Fix icon chat

--- App/Assets/Script/View/MessageAvatarItemView.cs
+++ App/Assets/Script/View/MessageAvatarItemView.cs
@@ -4,13 +4,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TMPro;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.UI;
 
 class MessageAvatarItemView : MonoBehaviour
 {
+    [SerializeField] private UserDataAsset userDataAsset;
+    [SerializeField] private ChatDataAsset chatDataAsset;
     [SerializeField] private Image image;
+    [SerializeField] private TextMeshProUGUI text;
     [SerializeField] private GameObject imageActive;
     [SerializeField] private Button button;
     [SerializeField] private Sprite defaultSpite;
@@ -18,13 +22,40 @@ class MessageAvatarItemView : MonoBehaviour
 
     public void SetUp(HeaderConversationDataModel conversationDataModel)
     {
+        var conversationFriend = CheckDirectChat(conversationDataModel.id);
+        if (conversationFriend.id != null && conversationFriend.id != string.Empty)
+        {
+            StartCoroutine(LoadImage(conversationFriend.avatar));
+            text.text = conversationFriend.username;
+        }
+        else
+        {
+            StartCoroutine(LoadImage(conversationDataModel.avatar));
+            text.text = conversationDataModel.name;
+        }
 
-        StartCoroutine(LoadImage(conversationDataModel.avatar));
 
         this.conversationDataModel = conversationDataModel;
 
         button.onClick.RemoveAllListeners();
         button.onClick.AddListener(OpenChat);
+    }
+
+    private ConversationUserDataModel CheckDirectChat(string conversationId)
+    {
+        foreach (var conversationData in chatDataAsset.ConversationList)
+        {
+            if (conversationData.Key.id != conversationId)
+                continue;
+
+            var data = conversationData.Key.users;
+            if (data.Count == 2)
+                for (int j = 0; j < data.Count; j++)
+                    if (data[j].id != userDataAsset.UserDataModel.id)
+                        return data[j];
+        }
+
+        return default;
     }
 
     private void OpenChat()
